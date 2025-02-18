275e4ed4f218689f543e11e5cc149101e6a024af
Ravi Khokhar <khokhar.ravikumar@gmail.com>
2025-02-18 10:48:58 -0500

    Updated profile details

--- src/ui/Profile.tsx
+++ src/ui/Profile.tsx
@@ -3,9 +3,20 @@ import { FaGithub } from "react-icons/fa";
 function Profile() {
   return (
     <div className="absolute z-10 items-center justify-center flex flex-col left-1/2 top-3/4 transform -translate-x-1/2 -translate-y-1/2">
-      <h1 className="text-5xl">Ravi Khokhar</h1>
-      <p className="text-xl py-2">Software Tester & Web Developer</p>
-      <a href="https://github.com/ravikhokhar23" target="_blank">
+      <h1 className="md:text-5xl sm:text-5xl text-3xl text-center">
+        Ravi Khokhar
+      </h1>
+      <p className="md:text-xl sm:text-xl text-base py-1 text-center">
+        Software Tester by profession
+      </p>
+      <p className="md:text-xl sm:text-xl text-base text-center">
+        Web Developer by interest
+      </p>
+      <a
+        href="https://github.com/ravikhokhar23"
+        target="_blank"
+        className="py-2"
+      >
         <FaGithub className="w-5 h-5" />
       </a>
     </div>
